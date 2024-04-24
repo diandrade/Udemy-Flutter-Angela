0c2cb2b3ed6e37d547e559e591cb4e180b4dcefc
Diego Andrade <contato.andradediego@gmail.com>
2024-04-24 10:58:25 -0300

    Destini Finished

--- Quizzler/quizzler/lib/main.dart
+++ Quizzler/quizzler/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart'; 
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:flutter/material.dart';
 import 'quiz_brain.dart';
